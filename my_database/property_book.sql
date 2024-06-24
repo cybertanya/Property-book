7eb38874317b035172ff78cfea07bd8b4c81a4a6
cybertanya <30558706+cybertanya@users.noreply.github.com>
2024-06-24 20:38:36 +0200

    Database upload
    Database upload

--- my_database/property_book.sql
+++ my_database/property_book.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jun 23, 2024 at 09:33 PM
+-- Generation Time: Jun 24, 2024 at 08:23 PM
 -- Server version: 10.4.32-MariaDB
 -- PHP Version: 8.2.12
 
@@ -33,14 +33,6 @@ CREATE TABLE `cache` (
   `expiration` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
---
--- Dumping data for table `cache`
---
-
-INSERT INTO `cache` (`key`, `value`, `expiration`) VALUES
-('tanyaradzwakavumbura@gmail.com|127.0.0.1', 'i:1;', 1719168658),
-('tanyaradzwakavumbura@gmail.com|127.0.0.1:timer', 'i:1719168658;', 1719168658);
-
 -- --------------------------------------------------------
 
 --
@@ -68,6 +60,49 @@ CREATE TABLE `failed_jobs` (
   `exception` longtext NOT NULL,
   `failed_at` timestamp NOT NULL DEFAULT current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `footer_contents`
+--
+
+CREATE TABLE `footer_contents` (
+  `id` bigint(20) UNSIGNED NOT NULL,
+  `main_name` varchar(255) NOT NULL,
+  `description` text NOT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Dumping data for table `footer_contents`
+--
+
+INSERT INTO `footer_contents` (`id`, `main_name`, `description`, `created_at`, `updated_at`) VALUES
+(1, 'We love to make perfect solutions for your business', 'Why l say old chap that is, spiffing off his nut cor blimey guvnords geeza bloke knees up bobby, sloshed arse William cack Richard fanny around chesed of bum bag old lost the pilot say there spiffing off his nut.', '2024-06-23 20:31:07', '2024-06-24 16:20:51');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `header_contents`
+--
+
+CREATE TABLE `header_contents` (
+  `id` bigint(20) UNSIGNED NOT NULL,
+  `main_name` varchar(255) NOT NULL,
+  `description` text NOT NULL,
+  `image` varchar(255) NOT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Dumping data for table `header_contents`
+--
+
+INSERT INTO `header_contents` (`id`, `main_name`, `description`, `image`, `created_at`, `updated_at`) VALUES
+(1, 'Corporate Website', 'We are a digital agency that helps brands to achieve their business outcomes. We see technology as a tool to create amazing things.', '1719251704.jpg', '2024-06-23 20:03:16', '2024-06-24 15:55:04');
 
 -- --------------------------------------------------------
 
@@ -123,7 +158,13 @@ CREATE TABLE `migrations` (
 INSERT INTO `migrations` (`id`, `migration`, `batch`) VALUES
 (1, '0001_01_01_000000_create_users_table', 1),
 (2, '0001_01_01_000001_create_cache_table', 1),
-(3, '0001_01_01_000002_create_jobs_table', 1);
+(3, '0001_01_01_000002_create_jobs_table', 1),
+(4, '2024_06_23_194806_create_header_contents_table', 2),
+(5, '2024_06_23_195151_create_story_contents_table', 2),
+(6, '2024_06_23_195424_create_services_contents_table', 2),
+(7, '2024_06_23_195851_create_pricing_contents_table', 2),
+(8, '2024_06_23_200043_create_footer_contents_table', 2),
+(9, '2024_06_24_180547_add_components_to_services_contents_table', 3);
 
 -- --------------------------------------------------------
 
@@ -136,6 +177,53 @@ CREATE TABLE `password_reset_tokens` (
   `token` varchar(255) NOT NULL,
   `created_at` timestamp NULL DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `pricing_contents`
+--
+
+CREATE TABLE `pricing_contents` (
+  `id` bigint(20) UNSIGNED NOT NULL,
+  `starter` varchar(255) NOT NULL,
+  `exclusive` varchar(255) NOT NULL,
+  `premium` varchar(255) NOT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Dumping data for table `pricing_contents`
+--
+
+INSERT INTO `pricing_contents` (`id`, `starter`, `exclusive`, `premium`, `created_at`, `updated_at`) VALUES
+(1, '0', '99', '150', '2024-06-23 20:30:01', '2024-06-24 15:41:15');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `services_contents`
+--
+
+CREATE TABLE `services_contents` (
+  `id` bigint(20) UNSIGNED NOT NULL,
+  `refreshing_design` varchar(255) NOT NULL,
+  `solid_bootstrap` varchar(255) NOT NULL,
+  `speed` varchar(255) NOT NULL,
+  `fully_customizable` varchar(255) NOT NULL,
+  `regular_updates` varchar(255) NOT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL,
+  `components` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Dumping data for table `services_contents`
+--
+
+INSERT INTO `services_contents` (`id`, `refreshing_design`, `solid_bootstrap`, `speed`, `fully_customizable`, `regular_updates`, `created_at`, `updated_at`, `components`) VALUES
+(1, 'Lorem ipsum dolor sit amet, adipiscing elit, sed diam nonumy eirmod tempor invidunt labor dolore magna.', 'Lorem ipsum dolor sit amet, adipiscing elit, sed diam nonumy eirmod tempor invidunt labor dolore magna.', 'Lorem ipsum dolor sit amet, adipiscing elit, sed diam nonumy eirmod tempor invidunt labor dolore magna.', 'Lorem ipsum dolor sit amet, adipiscing elit, sed diam nonumy eirmod tempor invidunt labor dolore magna.', 'Lorem ipsum dolor sit amet, adipiscing elit, sed diam nonumy eirmod tempor invidunt labor dolore magna.', '2024-06-23 20:29:06', '2024-06-24 16:12:03', 'Lorem ipsum dolor sit amet, adipiscing elit, sed diam nonumy eirmod tempor invidunt labor dolore magna.');
 
 -- --------------------------------------------------------
 
@@ -157,7 +245,32 @@ CREATE TABLE `sessions` (
 --
 
 INSERT INTO `sessions` (`id`, `user_id`, `ip_address`, `user_agent`, `payload`, `last_activity`) VALUES
-('8ux0eWyILbXoydtqGlgypdIKfNMtuSP1JCn2dcTb', 1, '127.0.0.1', 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/126.0.0.0 Safari/537.36', 'YTo0OntzOjY6Il90b2tlbiI7czo0MDoiWkg4c3B1SnRnTVhicjNWWjVKdlNEUW9ZeTNwNk0xdXZodUpqVHBCZiI7czo5OiJfcHJldmlvdXMiO2E6MTp7czozOiJ1cmwiO3M6MzE6Imh0dHA6Ly8xMjcuMC4wLjE6ODAwMC9kYXNoYm9hcmQiO31zOjY6Il9mbGFzaCI7YToyOntzOjM6Im9sZCI7YTowOnt9czozOiJuZXciO2E6MDp7fX1zOjUwOiJsb2dpbl93ZWJfNTliYTM2YWRkYzJiMmY5NDAxNTgwZjAxNGM3ZjU4ZWE0ZTMwOTg5ZCI7aToxO30=', 1719171168);
+('7LvWvN7ssuKsjtNzeVsbZDC4IrY77zKzXk5pV8un', NULL, '127.0.0.1', 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/126.0.0.0 Safari/537.36 Edg/126.0.0.0', 'YTo0OntzOjY6Il90b2tlbiI7czo0MDoieXVld0FEbGdFNzdCVU04ckZueWRVNW9kZUdrc1hNM25pNGNZVDQ5UCI7czozOiJ1cmwiO2E6MTp7czo4OiJpbnRlbmRlZCI7czozNToiaHR0cDovLzEyNy4wLjAuMTo4MDAwL3N0b3J5X2NvbnRlbnQiO31zOjk6Il9wcmV2aW91cyI7YToxOntzOjM6InVybCI7czoyNzoiaHR0cDovLzEyNy4wLjAuMTo4MDAwL2xvZ2luIjt9czo2OiJfZmxhc2giO2E6Mjp7czozOiJvbGQiO2E6MDp7fXM6MzoibmV3IjthOjA6e319fQ==', 1719239802),
+('b2VsUsoWrTW5HGXZ2NbVytmXmJ9w0qX9Aq4D3KBS', 1, '127.0.0.1', 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/126.0.0.0 Safari/537.36', 'YTo0OntzOjY6Il90b2tlbiI7czo0MDoibGY0RGs0UkswN0xNQnlTdWJPRXlHQnJVOVpEZlZESDA1aVFqRDhNbSI7czo5OiJfcHJldmlvdXMiO2E6MTp7czozOiJ1cmwiO3M6MjE6Imh0dHA6Ly8xMjcuMC4wLjE6ODAwMCI7fXM6NjoiX2ZsYXNoIjthOjI6e3M6Mzoib2xkIjthOjA6e31zOjM6Im5ldyI7YTowOnt9fXM6NTA6ImxvZ2luX3dlYl81OWJhMzZhZGRjMmIyZjk0MDE1ODBmMDE0YzdmNThlYTRlMzA5ODlkIjtpOjE7fQ==', 1719253255);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `story_contents`
+--
+
+CREATE TABLE `story_contents` (
+  `id` bigint(20) UNSIGNED NOT NULL,
+  `main_name` varchar(255) NOT NULL,
+  `who_are_we` text NOT NULL,
+  `our_vision` text NOT NULL,
+  `history` text NOT NULL,
+  `image` varchar(255) NOT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Dumping data for table `story_contents`
+--
+
+INSERT INTO `story_contents` (`id`, `main_name`, `who_are_we`, `our_vision`, `history`, `image`, `created_at`, `updated_at`) VALUES
+(1, 'Our team comes with the experience and knowledge', 'It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, look like readable English.\r\n\r\nThere are many variations of passages of Lorem Ipsum available, but the majority have in some form, by injected humour.', 'It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, look like readable English.There are many variations of passages of Lorem Ipsum available, but the majority have in some form, by injected humour.', 'It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, look like readable English.There are many variations of passages of Lorem Ipsum available, but the majority have in some form, by injected humour.', '1719251731.jpg', '2024-06-23 20:27:31', '2024-06-24 15:55:31');
 
 -- --------------------------------------------------------
 
@@ -181,7 +294,7 @@ CREATE TABLE `users` (
 --
 
 INSERT INTO `users` (`id`, `name`, `email`, `email_verified_at`, `password`, `remember_token`, `created_at`, `updated_at`) VALUES
-(1, 'Tanyaradzwa Kavumbura', 'tanyaradzwakavumbura@gmail.com', NULL, '$2y$12$6bv3u5/i9PNgc7HFQVC8NuP1RgCvVxOevws2K6cKu46t/19l038L.', NULL, '2024-06-23 17:01:14', '2024-06-23 17:01:14');
+(1, 'Tanyaradzwa Kavumbura', 'tanyaradzwakavumbura@gmail.com', NULL, '$2y$12$6bv3u5/i9PNgc7HFQVC8NuP1RgCvVxOevws2K6cKu46t/19l038L.', 'gnhUsaRMoGGJ0B5z6u7SRIwlUsiT2mAl4cz5HxdGONWz44k2OxKiB0a8qluo', '2024-06-23 17:01:14', '2024-06-23 17:01:14');
 
 --
 -- Indexes for dumped tables
@@ -205,6 +318,18 @@ ALTER TABLE `cache_locks`
 ALTER TABLE `failed_jobs`
   ADD PRIMARY KEY (`id`),
   ADD UNIQUE KEY `failed_jobs_uuid_unique` (`uuid`);
+
+--
+-- Indexes for table `footer_contents`
+--
+ALTER TABLE `footer_contents`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `header_contents`
+--
+ALTER TABLE `header_contents`
+  ADD PRIMARY KEY (`id`);
 
 --
 -- Indexes for table `jobs`
@@ -232,12 +357,30 @@ ALTER TABLE `password_reset_tokens`
   ADD PRIMARY KEY (`email`);
 
 --
+-- Indexes for table `pricing_contents`
+--
+ALTER TABLE `pricing_contents`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `services_contents`
+--
+ALTER TABLE `services_contents`
+  ADD PRIMARY KEY (`id`);
+
+--
 -- Indexes for table `sessions`
 --
 ALTER TABLE `sessions`
   ADD PRIMARY KEY (`id`),
   ADD KEY `sessions_user_id_index` (`user_id`),
   ADD KEY `sessions_last_activity_index` (`last_activity`);
+
+--
+-- Indexes for table `story_contents`
+--
+ALTER TABLE `story_contents`
+  ADD PRIMARY KEY (`id`);
 
 --
 -- Indexes for table `users`
@@ -257,6 +400,18 @@ ALTER TABLE `failed_jobs`
   MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT;
 
 --
+-- AUTO_INCREMENT for table `footer_contents`
+--
+ALTER TABLE `footer_contents`
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `header_contents`
+--
+ALTER TABLE `header_contents`
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
 -- AUTO_INCREMENT for table `jobs`
 --
 ALTER TABLE `jobs`
@@ -266,7 +421,25 @@ ALTER TABLE `jobs`
 -- AUTO_INCREMENT for table `migrations`
 --
 ALTER TABLE `migrations`
-  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+
+--
+-- AUTO_INCREMENT for table `pricing_contents`
+--
+ALTER TABLE `pricing_contents`
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `services_contents`
+--
+ALTER TABLE `services_contents`
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `story_contents`
+--
+ALTER TABLE `story_contents`
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
 -- AUTO_INCREMENT for table `users`
